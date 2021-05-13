12cbd3cf19ff15be812148e6c10e3f4a2309f719
Kr9091 <nkranz@yahoo.com>
2021-05-13 15:10:51 -0700

    Created Layout, Header, and Footer

--- pages/events/[slug].js
+++ pages/events/[slug].js
@@ -1,7 +1,8 @@
+import Layout from '../../components/Layout';
 export default function EventPage() {
   return (
-    <div>
+    <Layout>
       <h1>My Event</h1>
-    </div>
+    </Layout>
   );
 }
