6b8367000eb205ce3919dc6f72b62a7f34eb0750
Kr9091 <nkranz@yahoo.com>
2021-05-13 17:55:55 -0700

    added static props for single event listing

--- pages/events/[slug].js
+++ pages/events/[slug].js
@@ -1,8 +1,44 @@
 import Layout from '@/components/Layout';
-export default function EventPage() {
+import { API_URL } from '@/config/index';
+export default function EventPage({ evt }) {
   return (
     <Layout>
-      <h1>My Event</h1>
+      <h1>{evt.name}</h1>
     </Layout>
   );
 }
+
+export async function getStaticPaths() {
+  const res = await fetch(`${API_URL}/api/events`);
+  const events = await res.json();
+
+  const paths = events.map((evt) => ({
+    params: { slug: evt.slug },
+  }));
+
+  return {
+    paths,
+    fallback: true,
+  };
+}
+
+export async function getStaticProps({ params: { slug } }) {
+  const res = await fetch(`${API_URL}/api/events/${slug}`);
+  const events = await res.json();
+  return {
+    props: {
+      evt: events[0],
+    },
+    revalidate: 1,
+  };
+}
+
+// export async function getServerSideProps({ query: { slug } }) {
+//   const res = await fetch(`${API_URL}/api/events/${slug}`);
+//   const events = await res.json();
+//   return {
+//     props: {
+//       evt: events[0],
+//     },
+//   };
+// }
