9236a728fa9d522c63ed9ee9484a4b8a279e6e42
Kr9091 <kr9091@gmail.com>
2021-05-30 00:13:35 -0700

    deleted extra text

--- components/Layout.js
+++ components/Layout.js
@@ -10,7 +10,7 @@ export default function Layout({ title, keywords, description, children }) {
   return (
     <div>
       <Head>
-        <title>{title}</title>'
+        <title>{title}</title>
         <meta name='description' content={description} />
         <meta name='keywords' content={keywords} />
       </Head>
