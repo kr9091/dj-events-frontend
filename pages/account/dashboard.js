7144f8372d989f219ecbe9f71f9d47016d997fad
Kr9091 <kr9091@gmail.com>
2021-05-31 14:30:34 -0700

    Added functionality to get individual user events

--- pages/account/dashboard.js
+++ pages/account/dashboard.js
@@ -1,8 +1,30 @@
 import Layout from '@/components/Layout';
-export default function DashboardPage() {
+import { API_URL } from '@/config/index';
+import { parseCookies } from '@/helpers/index';
+
+export default function DashboardPage({ events }) {
   return (
     <Layout title='User Dashboard'>
       <h1>Dashboard</h1>
     </Layout>
   );
+}
+
+export async function getServerSideProps({ req }) {
+  const { token } = parseCookies(req);
+
+  const res = await fetch(`${API_URL}/events/me`, {
+    method: 'GET',
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  });
+
+  const events = await res.json();
+
+  return {
+    props: {
+      events,
+    },
+  };
 }
