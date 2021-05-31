058760f7bd36eae023c245d0e63064ef1372ca9a
Kr9091 <kr9091@gmail.com>
2021-05-31 14:57:33 -0700

    Display Dashboard Events

--- pages/account/dashboard.js
+++ pages/account/dashboard.js
@@ -1,11 +1,24 @@
 import Layout from '@/components/Layout';
+import DashboardEvent from '@/components/DashboardEvent';
 import { API_URL } from '@/config/index';
 import { parseCookies } from '@/helpers/index';
+import styles from '@/styles/Dashboard.module.css';
 
 export default function DashboardPage({ events }) {
+  const deleteEvent = (id) => {
+    console.log(id);
+  };
+
   return (
     <Layout title='User Dashboard'>
-      <h1>Dashboard</h1>
+      <div className={styles.dash}>
+        <h1>Dashboard</h1>
+        <h3>My Events</h3>
+
+        {events.map((evt) => (
+          <DashboardEvent key={evt.id} evt={evt} handleDelete={deleteEvent} />
+        ))}
+      </div>
     </Layout>
   );
 }
