c7d8fa42187accc4af68586fa623b0ab20bbeac9
Kr9091 <kr9091@gmail.com>
2021-06-04 01:11:58 -0700

    Authenticated Edit, Delete,  and Image Upload

--- pages/events/[slug].js
+++ pages/events/[slug].js
@@ -1,43 +1,16 @@
-import { ToastContainer, toast } from 'react-toastify';
+import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
-import { FaPencilAlt, FaTimes } from 'react-icons/fa';
+
 import Layout from '@/components/Layout';
 import { API_URL } from '@/config/index';
 import styles from '@/styles/Event.module.css';
 import Link from 'next/link';
 import Image from 'next/image';
-import { useRouter } from 'next/router';
 
 export default function EventPage({ evt }) {
-  const router = useRouter();
-  const deleteEvent = async (e) => {
-    if (confirm('Are you sure')) {
-      const res = await fetch(`${API_URL}/events/${evt.id}`, {
-        method: 'DELETE',
-      });
-
-      const data = await res.json();
-
-      if (!res.ok) {
-        toast.error(data.message);
-      } else {
-        router.push('/events');
-      }
-    }
-  };
   return (
     <Layout>
       <div className={styles.event}>
-        <div className={styles.controls}>
-          <Link href={`/events/edit/${evt.id}`}>
-            <a>
-              <FaPencilAlt /> Edit Event
-            </a>
-          </Link>
-          <a href='#' className={styles.delete} onClick={deleteEvent}>
-            <FaTimes /> Delete Event
-          </a>
-        </div>
         <span>
           {new Date(evt.date).toLocaleDateString('en-US')} at {evt.time}
         </span>
--- pages/events/edit/[id].js
+++ pages/events/edit/[id].js
@@ -1,3 +1,4 @@
+import { parseCookies } from '@/helpers/index';
 import { FaImage } from 'react-icons/fa';
 import moment from 'moment';
 import { ToastContainer, toast } from 'react-toastify';
@@ -12,7 +13,7 @@ import Image from 'next/image';
 import { API_URL } from '@/config/index';
 import styles from '@/styles/Form.module.css';
 
-export default function EditEventPage({ evt }) {
+export default function EditEventPage({ evt, token }) {
   const [values, setValues] = useState({
     name: evt.name,
     performers: evt.performers,
@@ -44,11 +45,16 @@ export default function EditEventPage({ evt }) {
       method: 'PUT',
       headers: {
         'Content-Type': 'application/json',
+        Authorization: `Bearer ${token}`,
       },
       body: JSON.stringify(values),
     });
 
     if (!res.ok) {
+      if (res.status === 403 || res.status === 401) {
+        toast.error('Unauthorized');
+        return;
+      }
       toast.error('Something Went Wrong');
     } else {
       const evt = await res.json();
@@ -170,21 +176,26 @@ export default function EditEventPage({ evt }) {
         </button>
       </div>
       <Modal show={showModal} onClose={() => setShowModal(false)}>
-        <ImageUpload evtId={evt.id} imageUploaded={imageUploaded} />
+        <ImageUpload
+          evtId={evt.id}
+          imageUploaded={imageUploaded}
+          token={token}
+        />
       </Modal>
     </Layout>
   );
 }
 
 export async function getServerSideProps({ params: { id }, req }) {
+  const { token } = parseCookies(req);
+
   const res = await fetch(`${API_URL}/events/${id}`);
   const evt = await res.json();
-
-  console.log(req.headers.cookie);
 
   return {
     props: {
       evt,
+      token,
     },
   };
 }
